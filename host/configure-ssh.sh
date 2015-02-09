730b022c4a283323e7a9a724139083f350ef8c1b
Kevin Gilpin <kgilpin@gmail.com>
2015-02-09 14:59:43 -0500

    adding two new files, not totally sure this is all tested

--- host/configure-ssh.sh
+++ host/configure-ssh.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 set -e
 
 /conjurize.sh
-
+apt-get update -y
 chef-solo -r https://github.com/conjur-cookbooks/conjur-ssh/releases/download/v1.2.5/conjur-ssh-v1.2.5.tar.gz -o role[configure]
 
 rm /etc/service/sshd/down
