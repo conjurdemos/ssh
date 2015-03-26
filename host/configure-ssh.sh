27fa1e7248e12f4e69d7d74304a8483fbd1347ce
Rafał Rzepecki <divided.mind@gmail.com>
2015-03-26 17:59:13 +0100

    Remove apt cleanup and update, disable cookbooks redownloading
    This is for quicker turnaround; those operations are mostly redundant.
    (Keeping the apt cache does make the image a bit bigger.)

--- host/configure-ssh.sh
+++ host/configure-ssh.sh
@@ -2,8 +2,7 @@
 set -e
 
 /conjurize.sh
-apt-get update -y
-chef-solo -r https://github.com/conjur-cookbooks/conjur-ssh/releases/download/v1.2.5/conjur-ssh-v1.2.5.tar.gz -o role[configure]
+chef-solo -o role[configure]
 
 rm /etc/service/sshd/down
 /etc/my_init.d/00_regen_ssh_host_keys.sh 
