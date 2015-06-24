5a66860fe66bc85cfb097649484aef92835fcb3c
Dustin Collins <dustinrcollins@gmail.com>
2015-06-24 15:12:07 -0400

    [#96673198] Track demo start

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+# Notify demo started
+REPORT=/tmp/report.sh
+wget -q https://raw.githubusercontent.com/conjurinc/demo-factory/master/report.sh -O ${REPORT}
+chmod a+x ${REPORT}
+bash /tmp/report.sh ssh
+
 if ! conjur authn whoami &> /dev/null || [ $(conjur authn whoami | jsonfield username) != demo ]; then
   conjur authn login -u demo -p demo
 fi
